4d99a33c06136a0a3597897ef12c9d3c15b61659
MattT <trojanface@hotmail.com>
2020-03-09 19:11:33 +1030

    Added a quiz page setup and gave the start button some limited functionality.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var startButton = document.getElementById("startButton");
 var startScreen = document.getElementById("startScreen");
+var quizScreen = document.getElementById("quizScreen");
 startButton.addEventListener("click", () => {
 startScreen.style.display = "none";
+quizScreen.style.display = "flex";
 });
