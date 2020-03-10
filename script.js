3ffdfdcc51c833470fb8ee2aaef5630c2e4898c3
MattT <trojanface@hotmail.com>
2020-03-10 11:57:09 +1030

    Added functionality to the answer buttons.

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 //Variables
-var quizQuestions = ["What is javascript?", "What is the proper syntax for an arrow function?", "What is a prompt box?", 
-"What does the keyword 'this' mean in javascript?", "What symbol should be used for comments in javascript?", 
-"What are the looping structures in javascript?", "When do you use escape characters?", 
-"How do you link a html document to a javascript script?", "What does the unshift method do?", 
-"What does the push method do?"];
+
+//Questions/Answers from https://youteam.io/blog/top-10-javascript-interview-questions-answers/
+var quizQuestions = ["What is javascript?", "What is the proper syntax for an arrow function?", "What is a prompt box?",
+    "What does the keyword 'this' mean in javascript?", "What symbol should be used for comments in javascript?",
+    "What are the looping structures in javascript?", "When do you use escape characters?",
+    "How do you link a html document to a javascript script?", "What does the unshift method do?",
+    "What does the push method do?"];
 var quizAnswers = [
-"An object-oriented programming language.", "A descendant of java.",
-"A combination of html & css.", "A markup language.",
+    "An object-oriented programming language.", "A descendant of java.",
+    "A combination of html & css.", "A markup language.",
 
-"() => { };", "function -> { }",
-"event arrowFunction { }", "(argument), => { };",
+    "() => { };", "function -> { }",
+    "event arrowFunction { }", "(argument), => { };",
 
-"An input box that removes control from the user.", "An input box.",
-"A prompt that offers a yes/no input.", "A box that is never late.",
+    "An input box that removes control from the user.", "An input box.",
+    "A prompt that offers a yes/no input.", "A box that is never late.",
 
-"'This' refers to the object where it was called.", "The current function.",
-"A reference to the current javascript script.", "A new variable.",
+    "'This' refers to the object where it was called.", "The current function.",
+    "A reference to the current javascript script.", "A new variable.",
 
-"// for single line and /* for multi-line.", "<!--",
-"```", "/",
+    "// for single line and /* for multi-line.", "<!--",
+    "```", "/",
 
-"For, while, do-while.", "For, if then, while.",
-"For each, return.", "Iterator, for, while.",
+    "For, while, do-while.", "For, if then, while.",
+    "For each, return.", "Iterator, for, while.",
 
-"When working with single/double quotes, apostrophes & ampersands.", "When we need to escape the current loop.",
-"To leave the vim.", "When calling the escape key.",
+    "When working with single/double quotes, apostrophes & ampersands.", "When we need to escape the current loop.",
+    "To leave the vim.", "When calling the escape key.",
 
-"<script src='script.js'></script>", "<script href='script.js'", 
-"<div src='script.js'>", "<link src='script'>",
+    "<script src='script.js'></script>", "<script href='script.js'",
+    "<div src='script.js'>", "<link src='script'>",
 
-"Attaches a new element to the beginning of the array.", "Removes the first array entry.",
-"Detects when the shift key is pressed.", "Shifts an array of elements down by 1.",
+    "Attaches a new element to the beginning of the array.", "Removes the first array entry.",
+    "Detects when the shift key is pressed.", "Shifts an array of elements down by 1.",
 
-"Attaches a new element to the end of the array.", "Creates a new element in the dom.",
-"Renames an array.", "Adds an element in the middle of an array."
+    "Attaches a new element to the end of the array.", "Creates a new element in the dom.",
+    "Renames an array.", "Adds an element in the middle of an array."
 
 ];
-var correctAnswers = [0,0,0,0,0,0,0,0,0,0,0];
+var correctAnswers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var currentQuestion = 0;
 
 var startButton = document.getElementById("startButton");
@@ -49,16 +51,60 @@ var quizA3 = document.getElementById("quizAnswer3");
 var quizA4 = document.getElementById("quizAnswer4");
 //Event Listeners
 
+var correctNum = 0;
+var incorrectNum = 0;
+var timer = 0;
+
 startButton.addEventListener("click", () => {
     startScreen.style.display = "none";
     quizScreen.style.display = "flex";
     gettinQuizzy(0);
 });
 
-function gettinQuizzy (questionNo) {
+function gettinQuizzy(questionNo) {
     quizDisplay.innerText = quizQuestions[questionNo];
-    quizA1.innerText = quizAnswers[questionNo*4];
-    quizA2.innerText = quizAnswers[questionNo*4+1];
-    quizA3.innerText = quizAnswers[questionNo*4+2];
-    quizA4.innerText = quizAnswers[questionNo*4+3];
+    quizA1.innerText = quizAnswers[questionNo * 4];
+    quizA2.innerText = quizAnswers[questionNo * 4 + 1];
+    quizA3.innerText = quizAnswers[questionNo * 4 + 2];
+    quizA4.innerText = quizAnswers[questionNo * 4 + 3];
 }
+
+quizA1.addEventListener("click", () => {
+    if (correctAnswers[currentQuestion] == 0) {
+        correctNum++;
+    } else {
+        incorrectNum++;
+    }
+    currentQuestion++;
+    gettinQuizzy(currentQuestion);
+});
+
+quizA2.addEventListener("click", () => {
+    if (correctAnswers[currentQuestion] == 1) {
+        correctNum++;
+    } else {
+        incorrectNum++;
+    }
+    currentQuestion++;
+    gettinQuizzy(currentQuestion);
+});
+
+quizA3.addEventListener("click", () => {
+    if (correctAnswers[currentQuestion] == 2) {
+        correctNum++;
+    } else {
+        incorrectNum++;
+    }
+    currentQuestion++;
+    gettinQuizzy(currentQuestion);
+});
+
+quizA4.addEventListener("click", () => {
+    if (correctAnswers[currentQuestion] == 3) {
+        correctNum++;
+    } else {
+        incorrectNum++;
+    }
+    currentQuestion++;
+    gettinQuizzy(currentQuestion);
+});
